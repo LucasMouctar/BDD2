fd303363c0a8a960c7dacf8839ae2edc480d0c0a
Clément PICOT <clemt.picot@gmail.com>
2020-04-27 11:00:33 +0200

    Added delete button in AdminAllMeetingWindow

--- BDD2/src/Application/AdminAllMeetingWindow.java
+++ BDD2/src/Application/AdminAllMeetingWindow.java
@@ -1,11 +1,13 @@
 package Application;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
@@ -37,7 +39,9 @@ public class AdminAllMeetingWindow extends JFrame
 	{
 		DefaultTableModel model = new DefaultTableModel();
 		
-		
+
+		model.addColumn("");
+		model.addColumn("");
 		model.addColumn("CLASSIFICATION");
 		model.addColumn("PRENOM");
 		model.addColumn("NOM");
@@ -52,29 +56,68 @@ public class AdminAllMeetingWindow extends JFrame
         model.addColumn("REGLEMENT");
         model.addColumn("DATE REGLEMENT");
 		
-		
+
+		JTable table = null;
 		try 
 		{
-			PreparedStatement stmt1 = conn.prepareStatement("SELECT CLASSIFICATION_CONSULTATION, PRENOM_PATIENT, NOM_PATIENT, DATEDEBUT_CRENEAU, DATEFIN_CRENEAU, POSTURE_CONSULTATION, MOT_CLEF_CONSULTATION, COMPORTEMENT_CONSULTATION, NOTEANXIETE_CONSULTATION, PRIX_CONSULTATION, TYPEREGLEMENT_CONSULTATION, DATEREGLEMENT_CONSULTATION FROM CONSULTATION CON JOIN PATIENT P ON CON.PATIENTID_PATIENT = P.PATIENTID_PATIENT JOIN CRENEAU CRE ON CRE.CRENEAUXID_CRENEAU = CON.CRENEAUXID_CRENEAU");
-		
+			PreparedStatement stmt1 = conn.prepareStatement("SELECT CLASSIFICATION_CONSULTATION, PRENOM_PATIENT, NOM_PATIENT, CON.CRENEAUXID_CRENEAU, CON.PATIENTID_PATIENT, DATEDEBUT_CRENEAU, DATEFIN_CRENEAU, POSTURE_CONSULTATION, MOT_CLEF_CONSULTATION, COMPORTEMENT_CONSULTATION, NOTEANXIETE_CONSULTATION, PRIX_CONSULTATION, TYPEREGLEMENT_CONSULTATION, DATEREGLEMENT_CONSULTATION FROM CONSULTATION CON JOIN PATIENT P ON CON.PATIENTID_PATIENT = P.PATIENTID_PATIENT JOIN CRENEAU CRE ON CRE.CRENEAUXID_CRENEAU = CON.CRENEAUXID_CRENEAU");
 			ResultSet rset1 = stmt1.executeQuery();
 			
-	        while(rset1.next())
-	        {
-	            model.addRow(new Object[]{rset1.getString("CLASSIFICATION_CONSULTATION"), rset1.getString("PRENOM_PATIENT"), rset1.getString("NOM_PATIENT"),rset1.getDate("DATEDEBUT_CRENEAU"), rset1.getTime("DATEDEBUT_CRENEAU"), rset1.getTime("DATEFIN_CRENEAU"), rset1.getString("POSTURE_CONSULTATION"), rset1.getString("MOT_CLEF_CONSULTATION"), rset1.getString("COMPORTEMENT_CONSULTATION"), rset1.getInt("NOTEANXIETE_CONSULTATION"), rset1.getInt("PRIX_CONSULTATION"), rset1.getString("TYPEREGLEMENT_CONSULTATION"),rset1.getDate("DATEREGLEMENT_CONSULTATION")});
+	        while(rset1.next()) {
+	            model.addRow(new Object[]{
+	            		"‎🗑",
+//	            		deleteButton,
+//	            		modifyButton,
+	            		rset1.getString("CLASSIFICATION_CONSULTATION"),
+	            		rset1.getString("PRENOM_PATIENT"),
+	            		rset1.getString("NOM_PATIENT"),
+	            		rset1.getDate("DATEDEBUT_CRENEAU"),
+	            		rset1.getTime("DATEDEBUT_CRENEAU"),
+	            		rset1.getTime("DATEFIN_CRENEAU"),
+	            		rset1.getString("POSTURE_CONSULTATION"),
+	            		rset1.getString("MOT_CLEF_CONSULTATION"),
+	            		rset1.getString("COMPORTEMENT_CONSULTATION"),
+	            		rset1.getInt("NOTEANXIETE_CONSULTATION"),
+	            		rset1.getInt("PRIX_CONSULTATION"),
+	            		rset1.getString("TYPEREGLEMENT_CONSULTATION"),
+	            		rset1.getDate("DATEREGLEMENT_CONSULTATION")
+        		});
+	            table = new JTable(model);
+	            
+	            String action = "DELETE FROM CONSULTATION WHERE CRENEAUXID_CRENEAU=" + rset1.getInt("CRENEAUXID_CRENEAU") + " AND PATIENTID_PATIENT=" + rset1.getInt("PATIENTID_PATIENT");
+	            new ButtonColumn(
+            		table,
+            		new AbstractAction() {
+						private static final long serialVersionUID = 1L;
+						// Supprime la consultation associée puis supprime tous les créneaux qui n'ont pas de consultations associées
+						@Override
+						public void actionPerformed(ActionEvent event) {
+							PreparedStatement stmt2;
+							ResultSet rset2;
+							try {
+								stmt2 = conn.prepareStatement(action);
+								rset2 = stmt2.executeQuery();
+								rset2.next();
+
+								stmt2 = conn.prepareStatement("DELETE FROM CRENEAU WHERE NOT EXISTS(SELECT NULL FROM CONSULTATION WHERE CONSULTATION.CRENEAUXID_CRENEAU = CRENEAU.CRENEAUXID_CRENEAU)");
+								rset2 = stmt2.executeQuery();
+								rset2.next(); 
+								System.out.println("ok");
+							} catch (SQLException e) {
+								JOptionPane.showMessageDialog(null, e.getMessage());
+							}
+						}
+					},
+            		0
+    			);
 	        }
-	        
-			JTable table = new JTable(model);
-			 
 			
 			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 			TableColumnAdjuster tca = new TableColumnAdjuster(table); // Using TableColumnAdjuster (opensource code) tool we can easily resize column width
 			tca.adjustColumns();
-			
 			add(new JScrollPane(table));
 		} 
-		catch (SQLException e) 
-		{
+		catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
 	}
